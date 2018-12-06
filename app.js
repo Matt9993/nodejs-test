85313681466efdcc1ae586c916628b731443a704
Matt9993 <hriczmate@gmail.com>
2018-12-06 15:46:58 +0100

    both endpoints are working

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express')
+var Message = require('./Message.js');
 var app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send(new Message('The endpoints you can reach: /helloworld, /secondroute'));
+});
+
+app.get('/helloworld', function (req, res) {
+  res.send(new Message('ezigen'));
+});
+
+app.get('/secondroute', function (req, res) {
+  res.send(new Message('success'));
 })
  
 app.listen(3000)
